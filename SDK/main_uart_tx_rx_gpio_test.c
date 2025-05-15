cb6a498fa740c3b3a6d6a5cd6e113519ec3db9f3
charlie2951 <90516512+charlie2951@users.noreply.github.com>
2025-05-15 12:02:49 +0530

    example: UART tx and rx along with LED

--- SDK/main_uart_tx_rx_gpio_test.c
+++ SDK/main_uart_tx_rx_gpio_test.c
@@ -1,11 +1,17 @@
-// C code to test UART receiver-transmitter along with gpio
+/* C code to test UART receiver-transmitter along with gpio
+This design is to accespt input from serial terminal such as PUTTY and
+print the pressed key and display the same in the on-board LED if any number
+between 0-9 is pressed.
+All pressed characters are displayed on serial terminal
+Check the serial port number properly and connect to 9600 baud
+*/
 
 #include <krv32.h>
 
 
 int main()
 {
-   
+   	//support 9600 baud rate only
 	uart_sendline("Press any key between 0 to 9: \n\r");
       
     while (1)
