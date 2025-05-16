49f059a660922ddbcbf3aeb56409f74b6ef710b8
charlie2951 <90516512+charlie2951@users.noreply.github.com>
2025-05-16 09:34:09 +0530

    Update main_uart_tx_rx_gpio_test.c

--- SDK/main_uart_tx_rx_gpio_test.c
+++ SDK/main_uart_tx_rx_gpio_test.c
@@ -7,19 +7,15 @@ Check the serial port number properly and connect to 9600 baud
 */
 
 #include <krv32.h>
-
-
 int main()
-{
-   	//support 9600 baud rate only
+{   
 	uart_sendline("Press any key between 0 to 9: \n\r");
       
     while (1)
     {
 	volatile uint32_t rx_data = uart_receive();
-        GCSR->GPIO_0 =  rx_data - 48; //convert to decimal digit of num
-         
-        uart_sendline("You pressed:  ");
+        digitalwrite(rx_data - 48); //convert to decimal digit of num
+        uart_sendline("\n\rYou pressed:  ");
         uart_send((char)rx_data);
 	uart_sendline("\n\r ");
         delay(100000);
