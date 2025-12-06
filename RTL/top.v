8b285cfc53d19234b2d5e31e7d372a7ee0fc7bc2
charlie2951 <90516512+charlie2951@users.noreply.github.com>
2025-12-06 23:20:37 +0530

    Update top.v

--- RTL/top.v
+++ RTL/top.v
@@ -1,142 +1,104 @@
-/*Top module of KRV-32 SOC V1.0
--Debug Log:
-(2/5/2025): Tested with counter and working
-(2/5/2025): UART TX and RX working
-13/5/2025: PWM IP integrated(4 channel), 500Hz freq, duty cycle 0-255
-14/5/2025: PWM IP working
-*/
-module top (
-    input clk,
-    input reset_n,
-    output  [7:0] LEDS,
-    output uart_tx,
+module top(
+    input rst, clk, boot,
     input uart_rx,
-    output pwm0, pwm1, pwm2, pwm3
-    
-);
-  
-   wire divclk;
-   wire mem_rstrb;
-   wire mem_ready;
-   wire [31:0] mem_addr;
-   wire [31:0] mem_wdata;
-   wire [3:0] mem_wstrb;
-   wire [31:0] mem_rdata, rdata_gpio, rdata_uart_tx, rdata_uart_rx, pwm_data;
-
-    wire ismem      = (mem_addr[31:28]==4'h0);
-    wire isgpio     = (mem_addr[31:28]==4'h1);
-    wire isuart_tx  = (mem_addr[31:28]==4'h2);
-    wire isuart_rx  = (mem_addr[31:28]==4'h3);
-    wire ispwm      = (mem_addr[31:28]==4'h4);
-
-    wire [31:0] processor_rdata = ismem? mem_rdata: 
-                                isgpio? rdata_gpio:
-                                isuart_tx? rdata_uart_tx: 
-                                isuart_rx? rdata_uart_rx:32'h0;
-//Program memory
-progmem mem0(
-      .clk(divclk),.rst(reset_n),
-      .addr(mem_addr),
-      .data_out(mem_rdata),
-      .rd_strobe(ismem & mem_rstrb),
-      .data_in(mem_wdata),
-      .wr_strobe({4{ismem}} & mem_wstrb)
+    output uart_tx,
+    output [5:0] leds
   );
+  wire [31:0] mem_rdata,led_rdata,boot_rdata, mem_wdata, addr;
+  wire rstrb;
+  wire [3:0] wr_strobe;
+  wire uart_status;
+  wire [31:0] uart_data;
+  wire [5:0] tmp_led;
+//select device
+  wire isMEM = (addr[31:28]==4'b0000);//0x0000_0000
+  wire isLED = (addr[31:28]==4'b0001);//0x1000_0000
+  wire isUART = (addr[31:28]==4'b0010);//0x2000_0000
+ // wire isUART_RXDATA= (addr[19:16]==4'b0101);//0x0005_0000
+ // wire isUART_TXSTATUS= (addr[19:16]==4'b0100);//0x0004_0000
+ // wire isUART_RXSTATUS= (addr[19:16]==4'b0111);//0x0007_0000
+//  wire isboot = (addr[19:16]==4'b1000);//0x0008_0000
+//Selecting input data to CPU from memory or peripheral devices based on address
+  wire [31:0] cpu_rdata = isMEM ? mem_rdata:
+                        isLED ? led_rdata:
+                        isUART ? uart_data:32'h0;
+                        //isboot ? boot_rdata:
+                        //(isUART_RXSTATUS|isUART_RXDATA) ? uart_rx_data: 
+                        // isUART_TXSTATUS ? uart_status: 32'h0;
+                         
+//Instantiate sub modules
+// Mapping CPU
+  KRV32 cpu0(
+        .rst(!rst), .clk(clk),
+        .boot(boot),
+        .mem_rdata(cpu_rdata),
+        .mem_addr(addr),
+        .cycle(),
+        .mem_rstrb(rstrb),
+        .mem_wdata(mem_wdata),
+        .mem_wstrb(wr_strobe)
+      );
 
-//KRV32 CPU
-cpu KRV32(
-.rst(reset_n),
-.clk(divclk),
-.mem_addr(mem_addr),
-.mem_wdata(mem_wdata),
-.mem_wstrb(mem_wstrb),
-.mem_rdata(processor_rdata),
-.mem_rstrb(mem_rstrb)
-
-);
-
-
-//LED gpio IP
-   gpio_ip gpio_unit(
-    // System
-    .clk(divclk),
-    .rst(!reset_n),
-    // GPIO_0.DATA
-    .csr_gpio_0_data_out(LEDS),
-
-    // Local Bus
-    .waddr({4'h0, mem_addr[27:0]}),
-    .wdata(mem_wdata),
-    .wen(isgpio & (|mem_wstrb)),
-    .wstrb({4{isgpio}} & mem_wstrb),
-    .wready(),
-    .raddr({4'h0, mem_addr[27:0]}),
-    .ren(isgpio & mem_rstrb),
-    .rdata(rdata_gpio),
-    .rvalid()
-    );
-
-//UART TX IP
-   uart_ip uart_unit(
-
-    // System
-    .clk(divclk),
-    .rst(!reset_n),
-    // Local Bus
-    .waddr({4'h0, mem_addr[27:0]}),
-    .wdata(mem_wdata),
-    .wen(isuart_tx & (|mem_wstrb)),
-    .wstrb(mem_wstrb),
-    .wready(),
-    .raddr({4'h0, mem_addr[27:0]}),
-    .ren(isuart_tx & mem_rstrb),
-    .rdata(rdata_uart_tx),
-    .rvalid(),
-    // uart tx
-    .o_uart_tx(uart_tx)
-    );
-
-//UART RX IP
-uart_ip_rx uart_rx_unit(
-    // System
-    .clk(divclk),
-    .rst(!reset_n),
-    // Local Bus
-    .waddr({4'h0, mem_addr[27:0]}),
-    .wdata(mem_wdata),
-    .wen(isuart_rx & (|mem_wstrb)),
-    .wstrb(mem_wstrb),
-    .wready(),
-    .raddr({4'h0, mem_addr[27:0]}),
-    .ren(isuart_rx & mem_rstrb),
-    .rdata(rdata_uart_rx),
-    .rvalid(),
-    // uart rx
-    .i_uart_rx(uart_rx)
-    );
+//Mapping Program memory
+  progmem mem0(
+            .rst(rst), .clk(clk),
+            .addr(addr),
+            .data_in(mem_wdata),
+            .rd_strobe(rstrb & isMEM),
+            .wr_strobe(wr_strobe & {4{isMEM}}),
+            .data_out(mem_rdata)
+          );
 
 
-//PWM unit
-    pwm_ip pwm_unit(
-// System
-    .clk(divclk),
-    .rst(!reset_n),
-    // Local Bus
-    .waddr({4'h0, mem_addr[27:0]}),
-    .wdata(mem_wdata),
-    .wen(ispwm & (|mem_wstrb)),
-    .wstrb(mem_wstrb),
-    .wready(),
-    .raddr({4'h0, mem_addr[27:0]}),
-    .ren(ispwm & mem_rstrb),
-    .rdata(pwm_data),
-    .rvalid(),
-    // pwm channel output
-    .pwm_ch0(pwm0),
-    .pwm_ch1(pwm1),
-    .pwm_ch2(pwm2),
-    .pwm_ch3(pwm3)
-);
+// Mapping LED GPIO
+  led_gpio led0(
+                .rst(rst), .clk(clk),
+                .addr(addr),
+                .data_in(mem_wdata),
+                .rd_strobe(rstrb & isLED),
+                .wr_strobe(wr_strobe & {4{isLED}}),
+                .data_out(led_rdata),
+                .leds(tmp_led)
+                );
 
-assign divclk = clk;
+assign leds = ~tmp_led;
+
+/*
+//Mappring UART TX GPIO
+ uart_gpio uart_tx0(.rst(!rst), .clk(clk),
+                .addr(addr),
+                .data_in(mem_wdata),
+                .rd_strobe(rstrb ),
+                .wr_strobe(wr_strobe),
+                .data_out(uart_status),
+                .uart_tx(uart_tx)
+                );
+
+
+// Mapping UART Receiver GPIO
+ uart_rx_gpio uart_rx0(.rst(!rst), .clk(clk),
+                .addr(addr),
+                .data_in(mem_wdata),
+                .rd_strobe(rstrb ),
+                .wr_strobe(wr_strobe),
+                .data_out(uart_rx_data),
+                .uart_rx(uart_rx)
+                );
+
+
+*/
+
+//Mapping UART MODULE, BAUD RATE=9600, CLK FERQ=27MHz for Tang9K board
+ UART_GPIO 
+#(.CLK_FRE(27), .BAUD_RATE(9600))
+                uart_0(.rst(rst), .clk(clk),
+                .addr(addr),
+                .data_in(mem_wdata),
+                .rd_strobe(rstrb ),
+                .wr_strobe(wr_strobe),
+                .data_out(uart_data),
+                .uart_tx(uart_tx),
+                .uart_rx(uart_rx)
+                );
+  
 endmodule
