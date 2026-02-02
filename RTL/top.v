1e30d733764010d786b17008dac607419685464c
charlie2951 <90516512+charlie2951@users.noreply.github.com>
2026-02-02 10:01:48 +0530

    Update top.v

--- RTL/top.v
+++ RTL/top.v
@@ -1,37 +1,44 @@
-//*** Top module contain UART, LED gpio and Program Memory ***//
 module top(
-    input rst, clk, boot,
+    input rst, clk,
+    input boot_en,
     input uart_rx,
     output uart_tx,
-    output [5:0] leds
+    output [7:0] leds
   );
-  wire [31:0] mem_rdata,led_rdata,boot_rdata, mem_wdata, addr;
+  wire [31:0] mem_rdata, mem_wdata, addr;
   wire rstrb;
   wire [3:0] wr_strobe;
-  wire uart_status;
-  wire [31:0] uart_data;
-  wire [5:0] tmp_led;
+  wire [31:0] led_rdata,uart_rx_data,uart_tx_data,uart_data,crypto_data;
+  wire [31:0] boot_rdata;
 //select device
-  wire isMEM = (addr[31:28]==4'b0000);//0x0000_0000
-  wire isLED = (addr[31:28]==4'b0001);//0x1000_0000
-  wire isUART = (addr[31:28]==4'b0010);//0x2000_0000
- // wire isUART_RXDATA= (addr[19:16]==4'b0101);//0x0005_0000
- // wire isUART_TXSTATUS= (addr[19:16]==4'b0100);//0x0004_0000
- // wire isUART_RXSTATUS= (addr[19:16]==4'b0111);//0x0007_0000
-//  wire isboot = (addr[19:16]==4'b1000);//0x0008_0000
+  wire isMEM = (addr[31:24]==8'h00);
+  wire isLED = (addr[31:24]==8'h10);
+  wire isBOOT =(addr[31:24]==8'hA0);
+ //UART data read back by cpu
+  wire isUART_RXDATA= (addr[31:24]==8'h50);
+  wire isUART_RXSTATUS= (addr[31:24]==8'h70);
+  wire isUART_TXSTATUS= (addr[31:24]==8'h40);
+ 
+  //Crypto data out read back by cpu
+  wire isenc_valid_out= (addr[31:24]==8'h05);
+  wire isenc_data_out= (addr[31:24]==8'h06);
+  wire isdec_valid_out= (addr[31:24]==8'h0b);
+  wire isdec_data_out= (addr[27:24]==8'h0c);
+  wire iscrypto = isenc_valid_out|isenc_data_out|isdec_valid_out|isdec_data_out;
+
 //Selecting input data to CPU from memory or peripheral devices based on address
-  wire [31:0] cpu_rdata = isMEM ? mem_rdata:
+ wire [31:0] cpu_rdata = isMEM ? mem_rdata:
+                         isBOOT ? boot_rdata:
                         isLED ? led_rdata:
-                        isUART ? uart_data:32'h0;
-                        //isboot ? boot_rdata:
-                        //(isUART_RXSTATUS|isUART_RXDATA) ? uart_rx_data: 
-                        // isUART_TXSTATUS ? uart_status: 32'h0;
-                         
+                        (isUART_RXSTATUS|isUART_RXDATA|isUART_TXSTATUS) ? uart_data:
+                        iscrypto?crypto_data:32'h0;
+
+
 //Instantiate sub modules
 // Mapping CPU
-  KRV32 cpu0(
+  cpu cpu0(
         .rst(!rst), .clk(clk),
-        .boot(boot),
+        .boot_en(boot_en),
         .mem_rdata(cpu_rdata),
         .mem_addr(addr),
         .cycle(),
@@ -42,7 +49,7 @@ module top(
 
 //Mapping Program memory
   progmem mem0(
-            .rst(rst), .clk(clk),
+            .rst(!rst), .clk(clk),
             .addr(addr),
             .data_in(mem_wdata),
             .rd_strobe(rstrb & isMEM),
@@ -50,49 +57,18 @@ module top(
             .data_out(mem_rdata)
           );
 
-
 // Mapping LED GPIO
-  led_gpio led0(
-                .rst(rst), .clk(clk),
+led_gpio led0(.rst(!rst), .clk(clk),
                 .addr(addr),
                 .data_in(mem_wdata),
                 .rd_strobe(rstrb & isLED),
                 .wr_strobe(wr_strobe & {4{isLED}}),
                 .data_out(led_rdata),
-                .leds(tmp_led)
+                .leds(leds)
                 );
 
-assign leds = ~tmp_led;
-
-/*
-//Mappring UART TX GPIO
- uart_gpio uart_tx0(.rst(!rst), .clk(clk),
-                .addr(addr),
-                .data_in(mem_wdata),
-                .rd_strobe(rstrb ),
-                .wr_strobe(wr_strobe),
-                .data_out(uart_status),
-                .uart_tx(uart_tx)
-                );
-
-
-// Mapping UART Receiver GPIO
- uart_rx_gpio uart_rx0(.rst(!rst), .clk(clk),
-                .addr(addr),
-                .data_in(mem_wdata),
-                .rd_strobe(rstrb ),
-                .wr_strobe(wr_strobe),
-                .data_out(uart_rx_data),
-                .uart_rx(uart_rx)
-                );
-
-
-*/
-
-//Mapping UART MODULE, BAUD RATE=9600, CLK FERQ=27MHz for Tang9K board
- UART_GPIO 
-#(.CLK_FRE(27), .BAUD_RATE(9600))
-                uart_0(.rst(rst), .clk(clk),
+//mapping uart wrapper gpio regs
+uart_wrapper uart_regs(.rst(!rst), .clk(clk),
                 .addr(addr),
                 .data_in(mem_wdata),
                 .rd_strobe(rstrb ),
@@ -101,6 +77,39 @@ assign leds = ~tmp_led;
                 .uart_tx(uart_tx),
                 .uart_rx(uart_rx)
                 );
-  
-endmodule
 
+
+//Mapping with Crypto wrapper
+CRYPTO_GPIO crypto_0(
+.rst(rst), .clk(clk),
+                .addr(addr),
+                .data_in(mem_wdata),
+                .rd_strobe(rstrb ),
+                .wr_strobe(wr_strobe),
+                .data_out(crypto_data)
+                
+);  
+
+// Instantiate Boot loader memory
+bootmem boot_0(
+.clk(clk),
+.rst(!rst),
+.addr(addr),
+.rd_strobe(rstrb & isBOOT),
+.data_out(boot_rdata),
+.data_in(mem_wdata),
+.wr_strobe(wr_strobe & {4{isBOOT}})
+);
+    
+/*
+    //connect ILA for debug(optional)
+ila_0 ILA(
+.clk(clk),
+.probe0(addr),
+.probe1(mem_rdata),
+.probe2(mem_wdata),
+.probe3(boot_rdata)
+);
+*/
+
+endmodule
