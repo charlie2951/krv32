e5ac8950fccf53c7e7350d1da8ccd7e9828293b7
charlie2951 <90516512+charlie2951@users.noreply.github.com>
2026-02-11 11:04:44 +0530

    Update gpio.c

--- SDK/include/gpio.c
+++ SDK/include/gpio.c
@@ -46,18 +46,18 @@ uint8_t digitalRead(uint8_t pin)
     return (uint8_t)((*GPIO_DATA >> pin) & 0x1);
 }
 
-/* ---------- New functions ---------- */
+// ---------- New functions ---------- //
 
 void pinToggle(uint8_t pin)//not working -bug
 {
     *GPIO_DATA ^= (0x00000001 << pin);
 }
 
-/*
- * Write 8-bit value to a GPIO port starting at base_pin
- * Example:
- *   digitalWritePort(8, 0xAA);  // GPIO8–GPIO15
- */
+
+ // Write 8-bit value to a GPIO port starting at base_pin
+ //Example:
+ //  digitalWritePort(8, 0xAA);  // GPIO8–GPIO15
+ //
 void digitalWritePort(uint8_t base_pin, uint8_t value)
 {
     uint32_t mask = (0x000000FF << base_pin);
@@ -65,4 +65,5 @@ void digitalWritePort(uint8_t base_pin, uint8_t value)
     *GPIO_DATA &= ~mask;                     // clear port
     *GPIO_DATA |= ((uint32_t)value << base_pin);
 }
+
 
