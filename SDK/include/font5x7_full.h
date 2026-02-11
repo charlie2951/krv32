f7e1debac95129090b35d699d9799c14f9351700
charlie2951 <90516512+charlie2951@users.noreply.github.com>
2026-02-11 11:03:29 +0530

    Update font5x7_full.h

--- SDK/include/font5x7_full.h
+++ SDK/include/font5x7_full.h
@@ -1,13 +1,36 @@
+/*
+MIT License
+
+Copyright (c) 2024-2026 Subir Maity
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 #ifndef FONT5X7_FULL_H
 #define FONT5X7_FULL_H
 
 #include <stdint.h>
 
-/*
- * Standard 5x7 font
- * ASCII 0x20 (space) to 0x7F (DEL)
- * Each character is 5 bytes wide
- */
+//
+ // Standard 5x7 font
+ // ASCII 0x20 (space) to 0x7F (DEL)
+ // Each character is 5 bytes wide
+ //
 
 static const uint8_t font5x7[96][5] = {
     {0x00,0x00,0x00,0x00,0x00}, // 0x20 ' '
@@ -115,4 +138,5 @@ static const uint8_t font5x7[96][5] = {
     {0x00,0x00,0x00,0x00,0x00}  // 0x7F DEL
 };
 
-#endif /* FONT5X7_H */
+#endif 
+
