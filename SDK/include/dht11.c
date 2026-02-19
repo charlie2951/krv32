ee824618d162f966538f607edca1e94f37ad0ff6
charlie2951 <90516512+charlie2951@users.noreply.github.com>
2026-02-19 23:58:19 +0530

    Update dht11.c

--- SDK/include/dht11.c
+++ SDK/include/dht11.c
@@ -1,3 +1,33 @@
+/*
+    MIT License
+
+Copyright (c) 2024-2026 Subir Maity
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/*
+Driver for DHT11 temperature and Humidity
+Version-1.0
+Author: Subir Maity
+*/
+
 #include "dht11.h"
 
 // Initialize DHT11 sensor
@@ -119,4 +149,5 @@ static char dht11_read_byte(void)
     *temperature = data[2];
     return 0;
 }
+
 
