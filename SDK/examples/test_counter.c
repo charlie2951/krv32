cc78c40cf0fe2a347d3b4768cf6a42cd0ade2b13
charlie2951 <90516512+charlie2951@users.noreply.github.com>
2026-02-22 12:58:59 +0530

    Update test_counter.c

--- SDK/examples/test_counter.c
+++ SDK/examples/test_counter.c
@@ -3,21 +3,21 @@
 #include "gpio.h"
 #include "delay.h"
 #include "uart.h"
-
+#define TIMER TIMER1 //using timer1 for delay
 //main func
 int main(void)
 {
         static uint32_t count_val;
     //Initialize timer in free running counter mode to count pulse
     //Note: after initialization, immediately the counting will start
-    timer_init(1);//counter mode
+    timer_init(TIMER,1);//counter mode
       
     while(1)
     {
      delay(2000000);
-    timer_getcount(&count_val);
+    timer_getcount(TIMER,&count_val);
      uart_sendline(UART1,"Count value:");
-     uart_sendnumber(UART1,count_val);
+     uart_sendnumber(UART1,count_val);//this may show neg number because in uart_sendnumber(), accept signed int
      uart_sendline(UART1,"\r\n");
     }
 
