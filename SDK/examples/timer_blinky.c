930fee2d76340e4963d18b8f4f9a5376f79e6afd
charlie2951 <90516512+charlie2951@users.noreply.github.com>
2026-02-22 13:06:54 +0530

    Update timer_blinky.c

--- SDK/examples/timer_blinky.c
+++ SDK/examples/timer_blinky.c
@@ -2,7 +2,7 @@
 #include "timer.h"
 #include "gpio.h"
 #include "uart.h"
-
+#define TIMER TIMER0 //using timer-0
 int main(void)
 { 
   
@@ -13,10 +13,10 @@ int main(void)
     {
          digitalWrite(9,1);
          uart_sendline(UART1,"writing high \r\n");
-         timer_delay_ms(1000);
+         timer_delay_ms(TIMER,1000);
         digitalWrite(9,0);
         uart_sendline(UART1,"writing low \r\n");
-        timer_delay_ms(2000);
+        timer_delay_ms(TIMER,2000);
     }
 
     return 0;
