019ec02120c6855ace7dcf5baa3d8fa56b4b8e9d
charlie2951 <90516512+charlie2951@users.noreply.github.com>
2026-02-22 12:41:39 +0530

    Update dht11.c

--- SDK/include/dht11.c
+++ SDK/include/dht11.c
@@ -1,33 +1,3 @@
-/*
-    MIT License
-
-Copyright (c) 2024-2026 Subir Maity
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
-/*
-Driver for DHT11 temperature and Humidity
-Version-1.0
-Author: Subir Maity
-*/
-
 #include "dht11.h"
 
 // Initialize DHT11 sensor
@@ -47,9 +17,9 @@ void dht11_start_signal(void)
 {  //uart_sendline(UART1, "DHT sensor initialized \r\n");
     pinMode(dht_pin, OUTPUT);
     digitalWrite(dht_pin, LOW);
-    timer_delay_ms(18);//18ms
+    timer_delay_ms(TIMER,18);//18ms
     digitalWrite(dht_pin, HIGH);
-    timer_delay_us(40);//40 micros
+    timer_delay_us(TIMER,40);//40 micros
     pinMode(dht_pin, INPUT);
 }
 
@@ -62,7 +32,7 @@ static char dht11_read_byte(void)
         while (digitalRead(dht_pin) == LOW);
             // uart_sendline(UART1, "DHT pin still low...waiting \r\n");
              
-        timer_delay_us(25); // this must be accurate for proper sampling 
+        timer_delay_us(TIMER,25); // this must be accurate for proper sampling 
 
         if (digitalRead(dht_pin) == HIGH)
             value |= (1 << (7 - i));
@@ -90,12 +60,12 @@ static char dht11_read_byte(void)
     if (digitalRead(dht_pin) == LOW)
     {
         //uart_sendline(UART1, "DHT pin goes low \r\n");
-        timer_delay_us(80);//check this
+        timer_delay_us(TIMER,80);//check this
 
         if (digitalRead(dht_pin) == HIGH)
         {
               //uart_sendline(UART1, "DHT pin goes high \r\n");
-            timer_delay_us(80); //check this
+            timer_delay_us(TIMER,80); //check this
 
             for (int i = 0; i < 5; i++)
             {
@@ -149,5 +119,4 @@ static char dht11_read_byte(void)
     *temperature = data[2];
     return 0;
 }
-
 
