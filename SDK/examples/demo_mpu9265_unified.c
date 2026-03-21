1694da4d64992247c53286d2428a1c47d127ca73
charlie2951 <90516512+charlie2951@users.noreply.github.com>
2026-03-21 20:19:04 +0530

    Update demo_mpu9265_unified.c

--- SDK/examples/demo_mpu9265_unified.c
+++ SDK/examples/demo_mpu9265_unified.c
@@ -8,7 +8,8 @@
 #include "uart.h"
 #include "spi.h"
 #include "delay.h"
-
+#include "printf.h" //for higher level API, print support via uart1
+/*
 //helper function to print temp via uart in 'C
 void print_temp(int32_t temp_val) {
     if (temp_val < 0)
@@ -19,11 +20,14 @@ void print_temp(int32_t temp_val) {
     
     int32_t whole = temp_val / 100;  // 295 / 10 = 29
     int32_t decimal = temp_val % 100; // 295 % 10 = 5
-    uart_sendnumber(UART1, whole);
-    uart_sendline(UART1,".");
-    uart_sendnumber(UART1, decimal);
+    //uart_sendnumber(UART1, whole);
+   // uart_sendline(UART1,".");
+   // uart_sendnumber(UART1, decimal);
+  //new API reduces the burden of printing
+  printf("%d.%d", whole,decimal);
    
 }
+*/
 
 int main() {
     MPU9265_Data sensor;
@@ -31,14 +35,15 @@ int main() {
     // To use SPI:
    // int status = MPU9265_Init(MPU_INTERFACE_SPI, 0);
     // OR to use I2C:
-     i2c_init(&I2C0, 199);
+     i2c_init(&I2C0, 499);
     int status= MPU9265_Init(MPU_INTERFACE_I2C, &I2C0);
 if (status != 0) {
-        uart_sendline(UART1,"Error: MPU9265 not found!\r\n");
+       // uart_sendline(UART1,"Error: MPU9265 not found!\r\n");
+  printf("Error: MPU9265 not found!\r\n");
         while(1); 
     }
-    uart_sendline(UART1,"MPU9265 Initialized Successfully.\r\n");
-    uart_sendline(UART1,"AccX, AccY, AccZ | GyroX, GyroY, GyroZ | MagX, MagY, MagZ | Temp('C)\r\n");
+   printf("MPU9265 Initialized Successfully.\r\n");
+   printf("AccX, AccY, AccZ | GyroX, GyroY, GyroZ | MagX, MagY, MagZ | Temp('C)\r\n");
    
     
 
@@ -58,6 +63,7 @@ if (status != 0) {
         int m_y = sensor.my;
         int m_z = sensor.mz;
         //print
+      /*
         uart_sendnumber(UART1,x_accel);
         uart_sendline(UART1," |");
         uart_sendnumber(UART1,y_accel);
@@ -77,9 +83,10 @@ if (status != 0) {
         uart_sendnumber(UART1,m_z);
         uart_sendline(UART1," |");
         print_temp(sensor.temp_c);
-        uart_sendline(UART1,"\r\n");
+        uart_sendline(UART1,"\r\n"); */
+      printf("\r\na_x:%d, a_y:%d, a_z:%d, g_x:%d, g_y=%d, g_z=%d, m_x=%d, m_y=%d, m_z=%d, Temp=%d.%d", x_accel,y_accel,z_accel,g_x,g_y,g_z,m_x,m_y,m_z,sensor.temp_c/100,sensor.temp_c%100);
         // Dummy delay
-        delay(500000);
+        delay(2000000);
     }
     return 0;
 }
